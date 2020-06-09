5f23f59f8d3a02e51cfcf61a46c1288c6742d30e
Djameleddine21 <hd_sebbagh@esi.dz>
2020-06-10 00:20:36 +0100

    add miss file in the UI

--- lib/screens/home/widgets/todo_item.dart
+++ lib/screens/home/widgets/todo_item.dart
@@ -1,6 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:todos/models/todo.dart';
-import 'package:intl/intl.dart';
 import '../../../constants.dart';
 
 class TodoItem extends StatefulWidget {
@@ -15,20 +15,44 @@ class TodoItem extends StatefulWidget {
 class _TodoItemState extends State<TodoItem> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10.0),
-      child: ListTile(
-        title: Text(widget.todo.title),
-        subtitle: Text(
-          widget.todo.description,
-          maxLines: 1,
-          softWrap: true,
-          overflow: TextOverflow.ellipsis,
+    // genertae Key widget of Dissmisble and make sur that the key is unique with 2 random numbers
+    final Key keyWidegt = Key(widget.todo.title +
+        Random().nextInt(1000).toString() +
+        Random().nextInt(100).toString());
+    //
+    return Dismissible(
+      key: keyWidegt,
+      direction: DismissDirection.endToStart,
+      dismissThresholds: {DismissDirection.endToStart: 0.95},
+      onDismissed: (_) {
+        //TODO : delete item
+      },
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 10.0),
+        child: Icon(Icons.delete, color: Colors.white, size: 30.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: ListTile(
+          title: Text(widget.todo.title),
+          subtitle: Text(
+            widget.todo.description,
+            maxLines: 1,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: Text(widget.todo.dateTime.toString().substring(0, 10)),
+          leading: widget.todo.isDone
+              ? Icon(Icons.check_box, color: greenColor)
+              : Icon(Icons.check_box_outline_blank, color: greenColor),
+          onTap: () {
+            this.setState(() {
+              widget.todo.isDone = !widget.todo.isDone;
+            });
+          },
         ),
-        trailing: Text(widget.todo.dateTime.toString().substring(0, 10)),
-        leading: widget.todo.isDone
-            ? Icon(Icons.check_box, color: greenColor)
-            : Icon(Icons.check_box_outline_blank, color: greenColor),
       ),
     );
   }
